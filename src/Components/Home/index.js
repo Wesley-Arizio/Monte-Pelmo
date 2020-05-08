ac7948762896ea96daea282405c82df85b6cdc58
WesleyGolembiewski <wesleyarizio26@gmail.com>
2020-05-07 22:09:54 -0300

    gallery done

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -2,13 +2,23 @@ import React from 'react';
 import  { MdKeyboardArrowDown } from 'react-icons/md';
 import './style.css';
 import background from '../../assets/background.jpg'
+import img1 from '../../assets/img1.jpg'
+import img2 from '../../assets/img2.jpg'
+import img3 from '../../assets/img3.jpg'
+import img4 from '../../assets/img4.jpg'
+import img5 from '../../assets/img5.jpg'
+import img6 from '../../assets/img6.jpg'
+import img7 from '../../assets/img7.jpg'
+import img8 from '../../assets/img8.jpg'
 
 export default function Home(){
     return(
     <div className="home-container">
+        
         <header className="container">
                 <img src={background} alt="background"/>
         </header>
+       
         <section className="container-phrases">
             <ul className="prhases-list">
                 <li className="prhase">
@@ -129,7 +139,6 @@ export default function Home(){
 
         </section>
         
-        
         <section className="container-info">
                 <div className="container-title__about ">
                     <h1>Sobre nós</h1>
@@ -138,9 +147,42 @@ export default function Home(){
                 <div className="container-text " >
                     <p>
                     Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                    </p>
+                    </p>  
                 </div>
         </section>
+        
+        <section className="container-gallery">
+            <div className="title-gallery">
+                <h1>Veja nosso trabalho</h1>
+            </div>
+            <div className="gallery">
+                <div className="container-image">
+                    <img src={img1} alt=""/>
+                </div>
+                <div className="container-image">
+                    <img src={img2} alt=""/>
+                </div>
+                <div className="container-image">
+                    <img src={img3} alt=""/>
+                </div>
+                <div className="container-image">
+                    <img src={img4} alt=""/>
+                </div>
+                <div className="container-image">
+                    <img src={img5} alt=""/>
+                </div>
+                <div className="container-image">
+                    <img src={img6} alt=""/>
+                </div>
+                <div className="container-image">
+                    <img src={img7} alt=""/>
+                </div>
+                <div className="container-image">
+                    <img src={img8} alt=""/>
+                </div>
+            </div>
+        </section>        
+
     </div>
     );
 }
